c64b73c0b53992f852faba51697f56cb05cd1be5
Arkevorkhat <Arkevorkhat@outlook.com>
2015-06-16 15:03:56 -0700

    Create ArkUtilsTR.lua
    Communications Lib

---
+++ ArkUtilsTR.lua
@@ -0,0 +1,31 @@
+vnTR=1
+DsT="AUST--CRAF--TOS|--ABCD--ARKU--TILS"
+ID=os.getComputerID()
+--ArkUtilsTR lib Begins Here
+
+function AUoCH() --ArkUtils(open)Channel
+end
+function AUmTR(MS="" local TK="") --ArkUtils(main)Transmit
+  if TK=="" then
+    TK=DsT --WARNING!! THIS IS HIGHLY INSECURE
+    print("Warning: ArkUtilsTR: 9: Using Default Token, Run AUaST(Token) or AUaST() to Resolve")
+  end
+end
+function AUmRC()--Arkutils(main)Recieve
+    if
+  end
+    elseif
+  end
+end
+function AUaAU() --ArkUtils(Auth)Authenticate
+end
+function AUaST(local TK="") --ArkUtils(Auth)Set Token (NOTE: Standard token format is 6 4 character blocks separated by --)
+  
+end
+function AUaDA() --ArkUtils(Auth)DeAuthenticate
+end
+function AUaUT() --ArkUtils(Auth)Unset Token
+AUmTR("Unset Token")
+end
+function AUaCT() --ArkUtils(Auth)Create Token
+end
